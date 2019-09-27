ce262f6ac90bec5c2adb012a69d1e07e24c7a744
19sully96 <35587622+19sully96@users.noreply.github.com>
2019-09-27 17:00:41 -0400

    Switched order of  X and Y

--- generatePIF.py
+++ generatePIF.py
@@ -14,12 +14,12 @@ Z = 1    # Current pifgen functions only allow 2D configurations
 
 ### Layer 1
 cell_type = 'Medium'
-layer1 = np.zeros((X,Y),dtype=int)
+layer1 = np.zeros((Y,X),dtype=int)
 pifgen.write_layer(layer1,cell_type,filename)
 
 ### Layer 2
 cell_type = 'Cells'
-layer2 = np.zeros((X,Y),dtype=int)
+layer2 = np.zeros((Y,X),dtype=int)
 ## uniform seed
 cell_width = 10
 cell_height = 10
@@ -34,7 +34,7 @@ pifgen.append_layer(layer2,cell_type,filename)
 
 ### Layer 3
 cell_type = 'Wall'
-layer3 = np.zeros((X,Y),dtype=int)
+layer3 = np.zeros((Y,X),dtype=int)
 ## vertical walls
 wall_width = 10
 wall1 = pifgen.get_id(layer2,1)
